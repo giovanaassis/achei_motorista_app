9cd827718164c81b5d45fe3b9991c426a313f8ed
giovanaassis <giovana_2011_11@hotmail.com>
2025-08-08 00:14:15 -0300

    refactor: made search page responsive

--- frontend/app/(main)/search/page.tsx
+++ frontend/app/(main)/search/page.tsx
@@ -3,14 +3,14 @@ import SearchFilters from "@/app/components/SearchFilters";
 
 export default function SearchPage() {
   return (
-    <section className="text-black-primary pl-10 py-10 w-[80%]">
+    <section className="text-black-primary pl-10 py-10 w-[90%]">
       <h1 className="text-3xl">Motoristas em &quot;Nova Iguaçu&quot;</h1>
 
       {/* SEARCH SECTION */}
       <SearchFilters />
 
       {/* DRIVERS SECTION */}
-      <div className="my-15 flex flex-col gap-y-10">
+      <div className="my-15 flex flex-col gap-10">
         <DriverCard />
         <DriverCard />
       </div>
--- frontend/app/components/DriverCard.tsx
+++ frontend/app/components/DriverCard.tsx
@@ -4,22 +4,24 @@ import { FaCarSide } from "react-icons/fa";
 
 function DriverCard() {
   return (
-    <div className="border-2 border-gray-600 w-fit flex items-center p-5 gap-15 rounded-xl">
+    <div className="border-2 border-gray-600 w-fit flex items-center justify-around p-5 gap-5 rounded-xl md:w-[70%] lg:w-[50%]">
       {/* DRIVER PROFILE IMAGE */}
-      <div className="rounded-full bg-gray-800 w-15 h-15 md:w-25 md:h-25"></div>
+      <div className="bg-gray-800 rounded-full w-15 h-15 md:w-25 md:h-25"></div>
+
       {/* DRIVER INFO */}
-      <div>
-        <div className="flex text-2xl items-center justify-between font-bold md:text-3xl">
-          <h3>Roberto Silva</h3>
-          <FaCarSide className="text-gray-primary" />
+      <div className="flex flex-col md:w-[250px]">
+        <div className="driver-card-info">
+          <h3 className="text-2xl lg:text-3xl">Roberto Silva</h3>
+          <FaCarSide className="text-gray-primary my-2 text-2xl" />
         </div>
 
-        <div className="flex items-center justify-between text-gray-primary text-lg gap-x-10 mt-3 md:text-xl">
+        <div className="driver-card-info text-lg">
           <span>Nova Iguaçu</span>
           <span className="text-green">Disponível agora</span>
         </div>
       </div>
 
+      {/* ARROW ICON */}
       <Link href="/profile">
         <ArrowRight className="rounded-full w-8 h-8 hover:bg-gray-500 transition duration-200 cursor-pointer" />
       </Link>
